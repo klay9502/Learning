406da3fbff4da8e483b3b535313e773940c98e8a
Klay9502 <clay9502@naver.com>
2021-12-23 16:21:05 +0900

    Add Code File

---
+++ 단계별 문제/7_문자열/9-2941/Croatia.cpp
@@ -0,0 +1,71 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int CheckChar(char* _ptrChar)
+{
+	if (*_ptrChar == 'c' || *_ptrChar == 'd' || *_ptrChar == 'l' || *_ptrChar == 'n' || *_ptrChar == 's' || *_ptrChar == 'z')
+	{
+		switch (*_ptrChar) {
+		case 'c':
+			if (*(_ptrChar + 1) == '=')
+				return 1;
+			if (*(_ptrChar + 1) == '-')
+				return 1;
+			break;
+		case 'd':
+			if (*(_ptrChar + 1) == 'z' && *(_ptrChar + 2) == '=')
+				return 2;
+			if (*(_ptrChar + 1) == '-')
+				return 1;
+			break;
+		case 'l':
+			if (*(_ptrChar + 1) == 'j')
+				return 1;
+			break;
+		case 'n':
+			if (*(_ptrChar + 1) == 'j')
+				return 1;
+			break;
+		case 's':
+			if (*(_ptrChar + 1) == '=')
+				return 1;
+			break;
+		case 'z':
+			if (*(_ptrChar + 1) == '=' && *(_ptrChar - 1) != 'd')
+				return 1;
+			break;
+		}
+	}
+
+	return 0;
+}
+
+int main(void)
+{
+	string ex_str[8] = {
+		"c=",
+		"c-",
+		"dz=",
+		"d-",
+		"lj",
+		"nj",
+		"s=",
+		"z=",
+	};
+	string str;
+	int count = 0;
+	int del_count = 0;
+
+	cin >> str;
+
+	for (int i = 0; i < str.size(); i++)
+	{
+		del_count += CheckChar(&str[i]);
+		count++;
+	}
+
+	cout << count - del_count;
+
+	return 0;
+}
