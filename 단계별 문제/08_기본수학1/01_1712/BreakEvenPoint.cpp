313aa1b40a21d07992400f0065e186009f0ea805
Klay9502 <clay9502@naver.com>
2021-12-24 17:18:18 +0900

    Add new cpp file

---
+++ 단계별 문제/08_기본수학1/01_1712/BreakEvenPoint.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+	int A, B, C;
+
+	cin >> A >> B >> C;
+
+	if (B >= C)
+	{
+		cout << -1 << endl;
+	}
+	else
+	{
+		cout << (A / (C - B)) + 1 << endl;
+	}
+
+	return 0;
+}
