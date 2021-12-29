357dbaf47e2f5b89aea28129ac79360da8f63997
Klay9502 <clay9502@naver.com>
2021-12-29 16:29:35 +0900

    Add new cpp file

---
+++ 단계별 문제/08_기본수학1/04-2869/Snail.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+	int a, b, v;
+
+	cin >> a >> b >> v;
+
+	// ((v - a) + (a - b) - 1) / (a - b)
+	cout << ((v - b - 1) / (a - b)) + 1 << endl;
+
+	return 0;
+}
