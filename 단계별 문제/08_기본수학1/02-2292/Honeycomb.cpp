43b531cb123fd8e8a2d7d35713e9c190e799beae
Klay9502 <clay9502@naver.com>
2021-12-29 15:23:25 +0900

    Add new cpp file

---
+++ 단계별 문제/08_기본수학1/02-2292/Honeycomb.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+	int n;
+	int temp = 1;
+	int count = 1;
+
+	cin >> n;
+
+	for (int i = 1; n > temp; i++)
+	{
+		temp += (6 * i);
+		count++;
+	}
+
+	cout << count << endl;
+
+	return 0;
+}
