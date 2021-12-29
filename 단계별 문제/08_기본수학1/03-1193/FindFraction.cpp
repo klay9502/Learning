28cebd4fda5ff5235ccd9321e0f5a2e3f8708634
Klay9502 <clay9502@naver.com>
2021-12-29 15:56:22 +0900

    Add new cpp file

---
+++ 단계별 문제/08_기본수학1/03-1193/FindFraction.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+	int x;
+	int up = 1;		// 분자
+	int down = 1;	// 분모
+	int count = 1;
+
+	cin >> x;
+
+	for (int i = 0; i < (x - 1); i++)
+	{
+		if (down == 1)
+		{
+			count++;
+			down = count;
+			up = 1;
+		}
+		else
+		{
+			down--;
+			up++;
+		}
+	}
+
+	if (!(count % 2))
+	{
+		cout << up << "/" << down << endl;
+	}
+	else
+	{
+		cout << down << "/" << up << endl;
+	}
+
+	return 0;
+}
