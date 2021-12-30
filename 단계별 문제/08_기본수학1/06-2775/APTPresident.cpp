c7d5337cda5dbd3961a89cf1a122edb36e549685
Klay9502 <clay9502@naver.com>
2021-12-30 14:21:19 +0900

    Add new cpp file

---
+++ 단계별 문제/08_기본수학1/06-2775/APTPresident.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main(void)
+{
+	int t;		// test case
+
+	cin >> t;
+
+	for (int i = 0; i < t; i++)
+	{
+		int k, n;		// k층에 n호
+		int people = 0;
+
+		cin >> k;
+		cin >> n;
+
+		int* under = new int[n];
+		int* floor = new int[n];
+		floor[0] = 1;
+
+		// zero floor initialize
+		for (int j = 1; j <= n; j++)
+		{
+			under[j - 1] = j;
+		}
+
+		// processing
+		for (int j = 0; j < k; j++)
+		{
+			for (int p = 1; p < n; p++)
+			{
+				floor[p] = under[p] + floor[p - 1];
+			}
+
+			copy(floor, floor + n, under);		// copy array
+		}
+
+		cout << floor[n-1] << endl;
+
+		delete[] floor;
+		delete[] under;
+	}
+
+	return 0;
+}
