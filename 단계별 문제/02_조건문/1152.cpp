85162993eee309708231d542a4592878a3848b79
Klay <clay9502@naver.com>
2022-07-14 20:00:10 +0900

    Add new cpp file
    1152번 문제.

---
+++ 단계별 문제/02_조건문/1152.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void)
+{
+	string str;
+	char* ptrStart = nullptr;
+	char* ptrCount = nullptr;
+	int count = 0;
+
+	getline(cin, str);
+
+	for (int i = 0; i < str.size(); i++)
+	{
+		ptrCount = &str[i];
+
+		if (str[i] != ' ' && str[i] != '\0' && ptrStart == nullptr)
+		{
+			ptrStart = &str[i];
+			count++;
+		}
+
+		if (*ptrCount == ' ' && ptrStart != nullptr)
+			ptrStart = nullptr;
+	}
+
+	cout << count << endl;
+
+	return 0;
+}
