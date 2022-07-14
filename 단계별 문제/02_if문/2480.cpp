7c1704b072098818b7f6c4ad5c7369df468533b1
Klay <clay9502@naver.com>
2022-07-14 16:57:56 +0900

    Add new cpp file
    if문 문제 추가.

---
+++ 단계별 문제/02_if문/2480.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+	int input[3];
+	int overlap_input;
+	int select_level = 0;
+	int result = 0;
+
+	cin >> input[0] >> input[1] >> input[2];
+
+	for (int i = 0; i < 3; i++)
+	{
+		int level = 1;
+
+		for (int j = 0; j < 3; j++)
+		{
+			if (input[i] == input[j] && i != j)
+			{
+				overlap_input = input[i];
+				level++;
+			}
+		}
+
+		if (level >= select_level)
+			select_level = level;
+	}
+
+	int max = 0;
+
+	switch (select_level)
+	{
+	case 1:			// 중복 없음
+		for (int i = 0; i < 3; i++)
+		{
+			if (input[i] > max)
+				max = input[i];
+
+			result = max * 100;
+		}
+		break;
+	case 2:			// 중복 숫자 2개
+		result = 1000 + (overlap_input * 100);
+		break;
+	case 3:			// 중복 숫자 3개
+		result = 10000 + (overlap_input * 1000);
+		break;
+	}
+
+	cout << result << endl;
+
+	return 0;
+}
---
+++ 단계별 문제/02_if문/2525.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+	int hour, min;
+	int input;
+
+	cin >> hour >> min;
+	cin >> input;
+
+	int pH = (min + input) / 60;
+	int pM = (min + input) % 60;
+
+	hour = (hour + pH) % 24;
+	min = pM;
+
+	cout << hour << " " << min << endl;
+
+	return 0;
+}
