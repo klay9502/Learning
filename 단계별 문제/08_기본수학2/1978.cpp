4d325cfa4686cbc0e576ac2e7332c78458a882e8
Klay <clay9502@naver.com>
2022-07-18 13:20:17 +0900

    Create 1978.cpp
    1978번 문제 소스코드.

---
+++ 단계별 문제/08_기본수학2/1978.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void)
+{
+	int n = 0;
+	int count = 0;
+	
+	cin >> n;
+
+	int* x = new int[n];
+
+	for (int i = 0; i < n; i++)
+	{
+		bool check = false;
+		cin >> x[i];
+		
+		for (int j = 2; j < x[i]; j++)
+		{
+			if (x[i] % j == 0)
+			{
+				check = true;
+				break;
+			}
+			else continue;
+		}
+
+		if (!check && x[i] != 1) count++;
+	}
+
+	cout << count << endl;
+
+	delete x;
+
+	return 0;
+}
