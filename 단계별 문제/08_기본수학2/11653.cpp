de018658c3fb08ef732065f2cc068856ef3444f6
Klay <clay9502@naver.com>
2022-07-21 13:50:31 +0900

    Create 11653.cpp
    11653번 문제 소스코드.

---
+++ 단계별 문제/08_기본수학2/11653.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+	int n = 0;
+	bool check = false;
+
+	cin >> n;
+
+	for (int i = 2; i <= n; i++)
+	{
+		if (n % i == 0)
+		{
+			n /= i;
+			cout << i << endl;
+			i = 1;
+		}
+	}
+	
+	return 0;
+}
