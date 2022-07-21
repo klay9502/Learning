3f23964c5f08b6c605435443a628168aa7c2f25b
Klay <clay9502@naver.com>
2022-07-21 13:33:44 +0900

    Create 2581.cpp
    2581번 문제 소스코드.

---
+++ 단계별 문제/08_기본수학2/2581.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+	// m <= x <= n
+	int n = 0;
+	int m = 0;
+	int count = 0;
+	int minValue;
+	int total = 0;
+
+	cin >> m;
+	cin >> n;
+
+	minValue = n;
+
+	for (int i = m; i <= n; i++)
+	{
+		bool check = false;
+
+		for (int j = 2; j < i; j++)
+		{
+			if (i % j == 0)
+			{
+				check = true;
+				break;
+			}
+			else continue;
+		}
+
+		if (!check && i != 1)
+		{
+			count++;
+			total += i;
+
+			if (minValue >= i)
+				minValue = i;
+		}
+	}
+
+	if (count != 0)
+		cout << total << endl << minValue;
+	else cout << -1 << endl;
+
+	return 0;
+}
