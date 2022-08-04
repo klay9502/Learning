fa992ce646de39c6334ac07ca511c1ef9b0e3909
Klay <clay9502@naver.com>
2022-08-04 19:09:49 +0900

    Create 4948.cpp
    4948번 문제 소스코드.

---
+++ 단계별 문제/08_기본수학2/4948.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int Eratos(int m, int n);
+
+int main(void)
+{
+	int n = 0;
+
+	// 처리속도 UP, C의 입출력 사용안됌!
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	// 1 <= n <= 123,456
+	while (1)
+	{
+		cin >> n;
+
+		if (n == 0)
+			return 0;
+
+		cout << Eratos(n, 2 * n) << endl;
+	}
+}
+
+int Eratos(int m, int n)
+{
+	int count = 0;
+
+	if (n <= 1) return -1;
+
+	bool* PrimeArray = new bool[n + 1];
+
+	for (int i = 2; i <= n; i++)
+		PrimeArray[i] = true;
+
+	for (int i = 2; i * i <= n; i++)
+	{
+		if (PrimeArray[i])
+			for (int j = i * i; j <= n; j += i)
+				PrimeArray[j] = false;
+	}
+
+	for (int i = 0; i <= n; i++)
+		if (PrimeArray[i] && i > m && i != 0 && i != 1)
+			count++;
+
+	delete[] PrimeArray;
+
+	return count;
+}
